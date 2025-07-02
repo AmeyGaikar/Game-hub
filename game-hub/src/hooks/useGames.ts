d8974ea9ebcbfdc1230f43d6295b5bff15ca51ad
Amey Gaikar <gaikaramey04@gmail.com>
2025-07-02 20:33:45 +0530

    Build game cards

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -2,32 +2,32 @@ import { CanceledError } from "axios";
 import apiClient from "../services/api-client";
 import { useEffect, useState } from "react";
 
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface gameFetchResponse {
+  count: number;
+  results: Game[];
+}
 const useGames = () => {
-  interface Game {
-    id: number;
-    name: string;
-  }
-
-  interface gameFetchResponse {
-    count: number;
-    results: Game[];
-  }
-
   const [games, setGames] = useState<Game[]>([]);
   const [errors, setError] = useState("");
-  
+
   useEffect(() => {
     const controller = new AbortController();
 
     apiClient
-      .get<gameFetchResponse>("/games", {signal: controller.signal})
+      .get<gameFetchResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
       });
 
-      return () => controller.abort();
+    return () => controller.abort();
   }, []);
 
   return { games, errors };

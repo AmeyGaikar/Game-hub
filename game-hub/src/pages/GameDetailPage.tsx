2517cfac26d100ca7dee8b7130dabd089cbe30c7
Amey Gaikar <gaikaramey04@gmail.com>
2025-09-15 22:07:11 +0530

    Build expandable text

--- game-hub/src/pages/GameDetailPage.tsx
+++ game-hub/src/pages/GameDetailPage.tsx
@@ -1,19 +1,24 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGame";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
-  if (isLoading) return <Spinner />;
 
+  if (isLoading) return <Spinner />;
   if (error || !game) throw error;
 
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>
+        {game.description_raw}
+      </ExpandableText>
+
     </>
   );
 };

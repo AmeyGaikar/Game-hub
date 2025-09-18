12b0613e54c0ef57fbff815d437bb1e09f969c7d
Amey Gaikar <gaikaramey04@gmail.com>
2025-09-18 20:52:53 +0530

    Fix the navbar

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,19 +1,21 @@
-import { HStack, Image} from "@chakra-ui/react"
-import logo from "../assets/logo.webp"
-import ColorModeSwitch from "./ColorModeSwitch"
-import SearchInput from "./SearchInput"
-import useGameStore from "@/store"
-
-
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
+import useGameStore from "@/store";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
-<HStack padding='10px'>
-    <Image src={logo} boxSize='60px'/>
-    <SearchInput/>
-    <ColorModeSwitch/>
-</HStack>
-)
-}
+    <HStack padding="10px">
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" objectFit={'cover'}/>
+      </Link>
+      <SearchInput />
+      <ColorModeSwitch />
+    </HStack>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- game-hub/src/components/SearchInput.tsx
+++ game-hub/src/components/SearchInput.tsx
@@ -7,15 +7,18 @@ import {
 } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameStore((s) => s.setSearchText);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current?.value);
+        navigate("/");
       }}
     >
       <InputGroup>
